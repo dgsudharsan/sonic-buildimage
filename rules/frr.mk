c4e830440f8fa11c763d427c96df08df7c1cbcab
dgsudharsan <sudharsand@nvidia.com>
2023-04-12 21:39:10 +0000

    FRR 8.3 upgrade

--- rules/frr.mk
+++ rules/frr.mk
@@ -1,9 +1,9 @@
 # FRRouting (frr) package
 
-FRR_VERSION = 8.2.2
+FRR_VERSION = 8.3.2
 FRR_SUBVERSION = 0
-FRR_BRANCH = frr/8.2
-FRR_TAG = frr-8.2.2
+FRR_BRANCH = frr/8.3
+FRR_TAG = frr-8.3.2
 export FRR_VERSION FRR_SUBVERSION FRR_BRANCH FRR_TAG
 
 
